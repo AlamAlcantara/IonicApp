f36014bd8e89229fa6e543993a40a5ff48c78ad3
Alam Alcantara <alam@C02LWRC6F8J2.local>
2019-08-01 09:44:49 -0400

    badge counter bug fixed

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import {Tarea, TareaId} from '../todos/todos.page';
-import { Observable,of, Observer } from 'rxjs';
+import {Tarea} from '../todos/todos.page';
+import { Observable} from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -11,35 +11,28 @@ import { map } from 'rxjs/operators';
 })
 export class TabsPage implements OnInit {
 
-  cantTareasIncompleta:number;
-
-  // Observador = {
-  //   next: x=> this.cantTareasIncompleta = x
-  // };
-  // objObservable = of(this.actualizarCantTareasIncompletas()); 
-
+  cantTareasIncompleta:number = 0;
   private tareasCollection: AngularFirestoreCollection<Tarea>;
 
   constructor(public db: AngularFirestore) {
     this.tareasCollection = db.collection('tareas',ref=>ref.where('completado','==',false));
-
-  //  this.objObservable.subscribe(this.Observador);    
   }
 
   ngOnInit(){
    //this.cantTareasIncompletas = this.actualizarCantTareasIncompletas();
+   this.actualizarCantTareasIncompletas().subscribe(data=>{
+     this.cantTareasIncompleta = data;
+     console.log(data);
+   }); 
   }
 
   actualizarCantTareasIncompletas():Observable<number>{
-    // this.tareasCollection.get().toPromise().then(data=>{
-    //   console.log(data);
-    //   return data.docs.length;
-    // });
     let res= this.tareasCollection
     .snapshotChanges().pipe(
-      map(res=> {return res.length})
+      map(res=> {
+        return res.length;
+      })
     );
-    console.log(res);
     return res;
   }
 }
